6ed88509030b4b6ea1a47e56fa266568079ad3df
ESTECHTI <e.s.tech@outlook.com>
2023-07-17 21:58:01 -0300

    Jogada especial Roque parte 1

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -198,7 +198,7 @@ public class ChessMatch {
 	  placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 	  placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 	  placeNewPiece('d', 1, new Queem(board, Color.WHITE));
-      placeNewPiece('e', 2, new King(board, Color.WHITE));
+      placeNewPiece('e', 2, new King(board, Color.WHITE, this));
       placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
       placeNewPiece('g', 1, new Knight(board, Color.WHITE));
       placeNewPiece('h', 2, new Rook(board, Color.WHITE));
@@ -215,7 +215,7 @@ public class ChessMatch {
       placeNewPiece('b', 8, new Knight(board, Color.BLACK));
       placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
       placeNewPiece('d', 8, new Queem(board, Color.BLACK));
-      placeNewPiece('e', 8, new King(board, Color.BLACK));
+      placeNewPiece('e', 8, new King(board, Color.BLACK, this));
       placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
       placeNewPiece('g', 8, new Knight(board, Color.BLACK));
       placeNewPiece('h', 8, new Rook(board, Color.BLACK));
